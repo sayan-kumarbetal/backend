d8a1be3e8864a1e5c5c82f559d35a0ce13618a14
sayan-kumarbetal <betalsayan44@gmail.com>
2025-09-28 10:33:35 +0530

    Fix cover image upload route to use correct field name

--- src/routes/user.routs.js
+++ src/routes/user.routs.js
@@ -45,7 +45,7 @@ router
   .patch(verifyJwt, upload.single("avatar"), updateUserAvatar);
 router
   .route("/cover-image")
-  .patch(verifyJwt, upload.single("/coverImage"), updateUserCoverImage);
+  .patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage);
 router.route("/c/:username").get(verifyJwt, getUserChannelProfile);
 router.route("/history").get(verifyJwt, getWatchHistory);
 
