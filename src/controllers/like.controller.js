226e65a3049463187e57fa8b3f33f8ed2d8b6d30
sayan-kumarbetal <betalsayan44@gmail.com>
2026-05-16 20:08:55 +0530

    fix: import Video model in like controller and correct field name in getLikedVideos aggregation

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -3,6 +3,7 @@ import { User } from "../models/user.model.js";
 import { Comment } from "../models/comment.model.js";
 import { Tweet } from "../models/tweet.model.js";
 import { Like } from "../models/like.model.js";
+import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -194,7 +195,7 @@ const getLikedVideos = asyncHandler(async (req, res) => {
     },
     {
       $addFields: {
-        video: { $first: "$videodetails" },
+        video: { $first: "$videoDetails" },
       },
     },
     {
